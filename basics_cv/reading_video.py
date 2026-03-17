fcd77d0eca6e73c62d6bbcdef465324c01859a2c
Akash Vishwakarma <akashv@cdac.in>
2026-03-17 15:57:13 +0530

    addd video reading

---
+++ basics_cv/reading_video.py
@@ -0,0 +1,24 @@
+import cv2
+
+vid_path = r"D:\akashvProfile-TESTO-recorded-InCDAC-Lab\thermal-data\girish-demo.wmv"
+
+
+cap = cv2.VideoCapture(vid_path)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+print("FPS:", fps)
+delay = round(fps)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    cv2.imshow("My Video Window", frame)
+
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
